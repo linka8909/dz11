c97343ffdee70f3618abe5e474433fca7d78ba43
linka8909 <97902685+linka8909@users.noreply.github.com>
2022-12-26 17:21:41 +0200

    Add files via upload

---
+++ 26,12.js
@@ -0,0 +1,10 @@
+let f = document.getElementById("so2").textContent;
+
+var s = document.getElementById("so3").textContent
+
+function butTon() {
+	document.getElementById("so2").textContent = "JavaScript";
+}
+function func() {
+	document.getElementById("so3").textContent = "World";
+}
